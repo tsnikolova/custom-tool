1dd3cb20eda5e1a0697e93570c475235d0272747
tsnikolova <tsnikolova@users.noreply.github.com>
2025-10-16 15:31:59 +0100

    Update custom-tool.js

--- custom-tool.js
+++ custom-tool.js
@@ -76,6 +76,56 @@ unlayer.registerTool({
     },
   },
 });
+unlayer.registerTool({
+  name: 'my_tool2',
+  label: 'My Tool2',
+  icon: 'fa-smile',
+  supportedDisplayModes: ['web', 'email'],
 
+  options: {
+    default: {
+      title: null,
+    },
+    text: {
+      title: 'Border Radius',
+      position: 1,
+      options: {
+        borderRadius: {
+          label: 'Border Radius',
+          defaultValue: '0',
+          widget: 'my_rounded-border', // custom property editor
+        },
+      },
+		buttonColors: {
+          value: {
+            color: '#FFFFFF',
+            backgroundColor: '#3AAEE0',
+            hoverColor: '#FFFFFF',
+            hoverBackgroundColor: '#3AAEE0',
+          },
+        },
+    },
+  },
+  values: {},
+  renderer: {
+    Viewer: unlayer.createViewer({
+      render(values) {
+        return `<div style="border-radius: ${values.borderRadius};background-color:#3AAEE0;">I am a custom tool.</div>`;
+      },
+    }),
+    exporters: {
+      web: function (values) {
+        return `<div style="border-radius: ${values.borderRadius};background-color:#3AAEE0;">I am a custom tool.</div>`;
+      },
+      email: function (values) {
+        return `<div style="border-radius: ${values.borderRadius};background-color:#3AAEE0;">I am a custom tool.</div>`;
+      },
+    },
+    head: {
+      css: function (values) {},
+      js: function (values) {},
+    },
+  },
+});
   console.log('✅ Column+ tool registered');
 })();
